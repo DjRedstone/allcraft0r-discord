189acbb895fae700cc677aa7d026e681b3ce0d50
Baptiste May <baptiste_may@ens.univ-artois.fr>
2024-03-26 08:07:28 +0100

    Adding error message in discord response

--- discord/Discord.js
+++ discord/Discord.js
@@ -183,10 +183,11 @@ class Discord {
                     await cmd.execute(interaction);
                 } catch (error) {
                     this.error(error);
+                    const res = { content: `There was an error while executing this command!\n\`\`\`${error}\`\`\``, ephemeral: true };
                     if (interaction.replied || interaction.deferred) {
-                        await interaction.followUp({ content: "There was an error while executing this command!", ephemeral: true });
+                        await interaction.followUp(res);
                     } else {
-                        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+                        await interaction.reply(res);
                     }
                 }
             });
